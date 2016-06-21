7f4ea9b1bf27225038abf1323d4f2bc04bef8394
yangzhixin <1414229244@qq.com>
2016-06-21 14:01:18 +0800

    Initial Commit

---
+++ imageio/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  imageio
+//
+//  Created by 杨志新 on 16/6/21.
+//  Copyright © 2016年 杨志新. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ imageio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  imageio
+//
+//  Created by 杨志新 on 16/6/21.
+//  Copyright © 2016年 杨志新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
